4d7996e4f5590fd65f0d298a4e37dd42202e1ab6
RJ Sheperd <rjsheperd@gmail.com>
2023-06-29 10:34:56 -0700

    Use Baptiste's answer from #clojure-dart

--- ffi/lib/cljd-out/acme/main.dart
+++ ffi/lib/cljd-out/acme/main.dart
@@ -4,29 +4,20 @@ import "../cljd/core.dart" as lcoc_core;
 import "dart:ffi" as d_ffi;
 import "dart:io" as d_io;
 import "package:path/path.dart" as p_path;
-import "dart:developer" as d_developer;
-
-// BEGIN get-lib-path
-dc.dynamic get_lib_path(){
-if(d_io.Platform.isMacOS){
-return p_path.join(d_io.Directory.current.path, "hello_world", "libhello.dylib", );
-}
-if(d_io.Platform.isWindows){
-return p_path.join(d_io.Directory.current.path, "hello_world", "hello_library", "Debug", "hello.dll", );
-}
-return p_path.join(d_io.Directory.current.path, "hello_world", "libhello.so", );
-}
-
-// END get-lib-path
 
 // BEGIN main
 dc.dynamic main(){
-final dc.dynamic lib_path$1=lcoa_main.get_lib_path();
-final d_ffi.DynamicLibrary lib$1=(d_ffi.DynamicLibrary.open((lib_path$1 as dc.String), ));
-final d_ffi.Pointer<d_ffi.NativeType> castable$1=(lib$1.lookup("hello_world", ));
-final d_ffi.Pointer<d_ffi.NativeFunction> casted$1=(castable$1.cast<d_ffi.NativeFunction>());
-final d_ffi.Pointer<d_ffi.NativeFunction> hello$UNDERSCORE_world$1=casted$1;
-return (lcoc_core.println.$_invoke$3(lib_path$1, lib$1, hello$UNDERSCORE_world$1, ));
+late final dc.String lib_path$1;
+if(d_io.Platform.isMacOS){
+lib_path$1=p_path.join(d_io.Directory.current.path, "hello_library", "libhello.dylib", );
+}else if(d_io.Platform.isWindows){
+lib_path$1=p_path.join(d_io.Directory.current.path, "hello_library", "Debug", "hello.dll", );
+}else{
+lib_path$1=p_path.join(d_io.Directory.current.path, "hello_library", "libhello.so", );
+}
+final d_ffi.DynamicLibrary dylib$1=(d_ffi.DynamicLibrary.open(lib_path$1, ));
+final dc.Function hello$1=(d_ffi.NativeFunctionPointer((dylib$1.lookup<d_ffi.NativeFunction<d_ffi.Void Function()>>("hello_world", )), ).asFunction());
+return hello$1();
 }
 
 // END main
